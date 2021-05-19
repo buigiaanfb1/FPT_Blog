01af111bdcec505b9ff88e39c87e1a36916e6e3d
Bui Gia An <buigiaanfb1@gmail.com>
2021-05-19 23:54:37 +0700

    fix modal

--- client/src/common/theme.js
+++ client/src/common/theme.js
@@ -3,6 +3,6 @@ import { createMuiTheme } from '@material-ui/core';
 export const theme = createMuiTheme({
   zIndex: {
     // set these to whatever, I just set the appbar to 1
-    // modal: 1050,
+    modal: 1050,
   },
 });
